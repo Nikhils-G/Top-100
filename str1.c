e39ff7d2c94db4d4d39e260e2138bb59995d2f9b
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2025-03-23 23:52:02 +0530

    Create str1.c

---
+++ str1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);  // Takes input (single word)
+    printf("You entered: %s", str);
+    return 0;
+}
